19cee7cf33cf02bbba1540c96099e0bd5ec37f60
NJeanine-creator <jnishimwe321@daviscollege.com>
2021-03-25 19:57:45 +0200

    Add pom xml file

--- src/test/java/DatabaseRule.java
+++ src/test/java/DatabaseRule.java
@@ -5,7 +5,7 @@ public class DatabaseRule extends ExternalResource {
 
     @Override
     protected void before() {
-        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test", "belyse", "belyse");
+        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test", "nishimwe", "nishimwe");
     }
 
     @Override
